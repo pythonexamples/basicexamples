ecf356e210fce080c087ebc3ce08bcf366895b83
pythonexamples <48149386+pythonexamples@users.noreply.github.com>
2019-03-02 14:08:36 +0530

    Create python-if-else-example.py

---
+++ python-if-else-example.py
@@ -0,0 +1,12 @@
+#basic python if-else example
+#############################
+a = 2
+b = 4
+
+if a<b:
+	print(a, 'is less than', b)
+else:
+	print(a, 'is not less than', b)
+    
+#ouptut
+#2 is less than 4
