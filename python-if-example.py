d69e5a57f1acccbc162b476b37bf5aaefefbebc8
pythonexamples <48149386+pythonexamples@users.noreply.github.com>
2019-03-02 14:06:20 +0530

    Create python-if-example.py

---
+++ python-if-example.py
@@ -0,0 +1,28 @@
+#simple python if example
+a = 2
+b = 5
+if a<b:
+	print(a, 'is less than', b)
+
+#output
+#2 is less than 5
+
+
+#python if example with multiple conditions in the expression
+a = 2
+b = 5
+c = 4
+if a<b and a<c:
+	print(a, 'is less than', b, 'and', c)
+
+#output  
+#2 is less than 5 and 4
+
+
+#python if example - special scenario
+a = 2
+if a:
+	print(a, 'is not zero')
+
+#output
+#2 is not zero.
